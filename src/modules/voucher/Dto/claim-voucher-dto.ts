53757b83bde9c5f8c2299031d4cf699355c2b0b7
Vu Duong <19521110@gm.uit.edu.vn>
2021-12-30 21:14:35 +0700

    fix missing voucherId

--- src/modules/voucher/Dto/claim-voucher-dto.ts
+++ src/modules/voucher/Dto/claim-voucher-dto.ts
@@ -9,32 +9,27 @@ export class ClaimVoucherDto {
   @IsString()
   @IsOptional()
   @Trim()
-  @IsUUID()
   readonly voucherId: string;
 
   @ApiProperty()
   @IsString()
   @IsOptional()
   @Trim()
-  @IsUUID()
   readonly packageId: string;
 
   @ApiProperty()
   @IsString()
   @Trim()
-  @IsUUID()
   readonly supplierId: string;
 
   @ApiProperty()
   @IsString()
   @Trim()
-  @IsUUID()
   readonly key: string;
 
   @ApiProperty()
   @IsString()
   @Trim()
-  @IsUUID()
   readonly citizenId: string;
 
   @ApiPropertyOptional()
